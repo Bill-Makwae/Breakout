98d5fd32055181b150803fa7f3a79a9d9e9dd104
Bill Makwae <45584549+Bill-Makwae@users.noreply.github.com>
2020-05-30 14:32:37 -0600

    Adding a circular paddle

--- sketch.js
+++ sketch.js
@@ -8,6 +8,9 @@ var paddle3;
 var floor;
 var blocks;
 var blockSize;
+var Category1 = 0x0001,
+      Category2 = 0x0002,
+      Category3 = 0x0003;
 
 const colors = ["Crimson", "Brown", "BlanchedAlmond", "Chocolate"]
 
@@ -40,10 +43,18 @@ function setup() {
   //The paddle has to be shaped differently in order to change the 
   //way the game is played.
   
+        
   
   
   
-  paddle = matter.makeBarrier(width/2, height*0.9, 2*blockSize, 40);
+  paddle = matter.makeBall(width/2, height*0.9, 1.5*blockSize, {
+    collisionFilter: {
+      category: Category2
+    },
+    inertia: Infinity,
+    isStatic: true,
+    restitution: 1
+  });
   paddle.body.restitution = 1.0;
   
   paddle.move = function(){
@@ -53,12 +64,24 @@ function setup() {
   };
   paddle.color = "Aqua";
 
-  // paddle2 = matter.makeBlock((width/2)+30, height*0.9, 2*blockSize, 40);
-  // paddle.body.inertia = Infinity;
+  // paddle2 = matter.makeBlock((width/2)+30, height*0.9, blockSize, 40, {
+  //   collisionFilter: {
+  //     mask: Category1
+  //   },
+  //   inertia: Infinity,
+  //   angle: 45
+  // });
+  
   // paddle2.body.angle = 45;
   // paddle2.color = "Aqua";
+  // paddle2.move = function(){
+  //   let mx = constrain(mouseX + (blockSize), 0, width);
+  //   let my = constrain(mouseY , 0.4*height, height)
+  //   this.setPosition(mx, my);
+  // }
 
-  // matter.connect(paddle, paddle2);
+
+  //matter.connect(paddle, paddle2);
 
 
   ///This is creating the game ball
@@ -69,7 +92,7 @@ function setup() {
   // floor = matter.makeBarrier(width / 2, height, width, 50);
   // floor.body.restitution = 1.0;
   runCollisions();
-  
+  matter.changeGravity(0, 0.5);
 }
 
 function draw() {
@@ -83,7 +106,9 @@ function draw() {
 
   fill(paddle.color);
   paddle.move();
+  
   paddle.show();
+  
   
 
 
@@ -107,10 +132,18 @@ function draw() {
 }
 
 function createBall(){
-  let ballT = matter.makeBall(width / 2, height/2, blockSize/2);
-  ballT.body.restitution = 1.0;
+  let ballT = matter.makeBall(width / 2, height/2, blockSize/2, {
+    collisionFilter:{
+      category: Category1
+    },
+    restitution: 1.0,
+    frictionAir: 0.0,
+    friction: 0,
+    frictionStatic: 0
+  });
+  // ballT.body.restitution = 1.0;
   ballT.body.frictionAir = 0;
-  ballT.body.frictionStatic = 0;
+  // ballT.body.frictionStatic = 0;
   ballT.color = "Blue";
   ballT.velocity = (30, 0);
   return ballT;
@@ -118,17 +151,32 @@ function createBall(){
 }
 
 function runCollisions(){
-  Matter.Events.on(engine, 'collisionActive', function(event) {
+  //using the built in collision detection to remove the blocks
+  Matter.Events.on(engine, 'collisionStart', function(event) {
     var pairs = event.pairs;
 
     for (var i = 0; i < pairs.length; i++) {
         var pair = pairs[i];
         if (pair.bodyA.label != "Circle Body"){
-          console.log(pair.bodyA)
+          if (pair.bodyA.position.y < (height*0.4)){
+            for(let j = 0; j < blocks.length; j++){
+              if(pair.bodyA == blocks[j].body){
+                matter.forget(blocks[j])
+                blocks.splice(j,1)
+              }
+            }
+          }
          
         }
         if (pair.bodyB.label != "Circle Body"){
-          
+          if (pair.bodyB.position.y < (height*0.4)){
+            for(let j = 0; j < blocks.length; j++){
+              if(pair.bodyB == blocks[j].body){
+                matter.forget(blocks[j].body)
+                blocks.splice(j,1)
+              }
+            }
+          }
         }
     }
   });
